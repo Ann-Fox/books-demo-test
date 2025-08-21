a7378468cc35afce3e86e9c969b189b76d72f0c6
Ann-Fox <fx8565@mail.ru>
2025-08-21 09:02:18 +0500

    3.6. Шаблоны loading, error и 404 - loading

--- app/routes/author.js
+++ app/routes/author.js
@@ -1,11 +1,18 @@
-import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
+import Route from "@ember/routing/route";
+import { inject as service } from "@ember/service";
+import { Promise } from "rsvp";
+import { later } from "@ember/runloop";
 
 export default Route.extend({
-  dataService: service('data'),
+  dataService: service("data"),
 
-  model(){
-    return this.get('dataService').getAuthors();
+  model() {
+    return new Promise((resolve) => {
+      later(async () => {
+        let authors = await this.get("dataService").getAuthors();
+        resolve(authors);
+      }, 3000);
+    });
   },
   // templateName: 'author-list', //принцип configuration - можно переопределить шаблон, если названия не совпадают (шаблон author-list.hbs)
 });
