5d14be0454900b8cc405aef4d349a1a7dd3bef72
Ann-Fox <fx8565@mail.ru>
2025-08-21 09:43:31 +0500

    3.6. Шаблоны loading, error и 404 - 404

--- app/router.js
+++ app/router.js
@@ -1,13 +1,14 @@
-import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
+import EmberRouter from "@ember/routing/router";
+import config from "./config/environment";
 
 const Router = EmberRouter.extend({
   location: config.locationType,
-  rootURL: config.rootURL
+  rootURL: config.rootURL,
 });
 
-Router.map(function() {
-  this.route('author');
+Router.map(function () {
+  this.route("author");
+  this.route("404", { path: "*path" }); //переход на страницу 404, все пути, которые явно не указаны в route
 });
 
 export default Router;
